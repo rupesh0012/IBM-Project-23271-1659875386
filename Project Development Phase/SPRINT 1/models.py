67a772f8022fb1d29e41090279d4a4d6a0e53035
Sahana2019 <116181181+Sahana2019@users.noreply.github.com>
2022-11-15 23:20:05 +0530

    Add files via upload

---
+++ Project Development Phase/SPRINT 1/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy import Column, Integer, String
+# from app import db
+
+engine = create_engine('sqlite:///database.db', echo=True)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+
+Base.metadata.create_all(bind=engine)
